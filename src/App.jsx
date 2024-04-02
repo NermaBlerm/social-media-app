aa48366494c56559258ece2874b21d33de04867c
Norma Blum <blum_nomra@yahoo.com>
2024-04-01 22:36:54 -0400

    Used state to manage

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Post from './components/Post';
+import Comment from './components/Comment';
+function App(){ 
+  return(
+    <div>
+      <h1>Fakebook!</h1>
+      
+        {/* CreatePostForm */}
+        
+        {/* Feed */}
+        
+        <Post/>
+
+    </div>
+  )
+}
+export default App
